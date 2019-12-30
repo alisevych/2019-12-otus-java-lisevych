74f59462997b6fe328227243013176a424e31121
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2019-12-30 18:20:26 +0300

    HW01: simple example with guava's BigIntegerMath.factorial();

--- hw01-maven/src/main/java/ru/otus/hw01/HelloOtus.java
+++ hw01-maven/src/main/java/ru/otus/hw01/HelloOtus.java
@@ -1,11 +1,32 @@
 package ru.otus.hw01;
 
+import com.google.common.math.BigIntegerMath;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class HelloOtus {
 
-    public static void main(String... args) {
-        System.out.println("Test jar");
+    public static void main(String... args) throws IOException {
+
+        int intReceived = getPositiveInteger();
+        System.out.format("Factorial of %d is %d\n", intReceived, BigIntegerMath.factorial(intReceived).longValue());
+
     }
 
+    private static int getPositiveInteger() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter positive integer: ");
+        Integer result = null;
+        while (result == null) {
+            try {
+                String input = br.readLine();
+                result = Integer.valueOf(input);
+            } catch (NumberFormatException e) {
+                System.out.print("\nINTEGER, please: ");
+            }
+        }
+        return result;
+    }
 }
