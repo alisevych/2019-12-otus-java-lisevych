b87b400fa57c475dee6ac4f6e4df54e66bdd7712
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2020-01-19 21:38:39 +0300

    new hw02-generics module added; class DIYarrayList draft started; methods to implement added;

---
+++ hw02-generics/src/test/java/ru/otus/hw02/DIYarrayListTest.java
@@ -0,0 +1,15 @@
+package ru.otus.hw02;
+
+import java.util.List;
+
+
+public class DIYarrayListTest  {
+
+    public static void fillDIYarrayListAndCalculateMiddleValue(){
+
+        //ToDo implement
+        System.out.print("DIYarrayList");
+
+    }
+
+}
