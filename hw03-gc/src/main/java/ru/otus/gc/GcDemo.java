d4bd0797b8031f1c48161243cf9638ab82389165
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2020-02-05 13:38:40 +0300

    HW03: GCDemo: removed info not related to my test runs;

--- hw03-gc/src/main/java/ru/otus/gc/GcDemo.java
+++ hw03-gc/src/main/java/ru/otus/gc/GcDemo.java
@@ -31,31 +31,6 @@ http://openjdk.java.net/jeps/158
 -XX:+UseG1GC
 */
 
-/*
-1)
-    default, time: 83 sec (82 without Label_1)
-2)
-    -XX:MaxGCPauseMillis=100000, time: 82 sec //Sets a target for the maximum GC pause time.
-3)
-    -XX:MaxGCPauseMillis=10, time: 91 sec
-
-4)
--Xms2048m
--Xmx2048m
-    time: 81 sec
-
-5)
--Xms5120m
--Xmx5120m
-    time: 80 sec
-
-5)
--Xms20480m
--Xmx20480m
-    time: 81 sec (72 without Label_1)
-
-*/
-
 public class GcDemo {
 
     private static final String youngAction = "end of minor GC";
