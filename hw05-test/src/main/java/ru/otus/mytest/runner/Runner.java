5e28ddc7c665437509c67d222f2c297bda2359ce
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2020-02-19 16:55:28 +0300

    HW05: Runner methods created;

--- hw05-test/src/main/java/ru/otus/mytest/runner/Runner.java
+++ hw05-test/src/main/java/ru/otus/mytest/runner/Runner.java
@@ -1,7 +1,25 @@
 package ru.otus.mytest.runner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
 
+    private List<Class<?>> testClasses = new ArrayList<>();
 
+    public Runner() {
+    }
+
+    public Runner(Class<?>... vararg) {
+        testClasses = List.of(vararg);
+    }
+
+    public void executeAll(){
+        testClasses.forEach(this::execute);
+    }
+
+    public void execute(Class testClass) {
+        //ToDo Implement consequence of steps
+    }
 
 }
