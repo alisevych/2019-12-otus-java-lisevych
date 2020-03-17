550240b16cd79c14609bbea14a4c6aafac76b198
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2020-03-17 16:35:05 +0300

    HW07: after review: generateID() added with Random and check for uniqueness;

--- hw06-atm/src/main/java/ru/otus/atm/AtmBuilder.java
+++ hw06-atm/src/main/java/ru/otus/atm/AtmBuilder.java
@@ -1,18 +1,16 @@
 package ru.otus.atm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class AtmBuilder {
 
     private static final AuthorizationModule authorization =  new AuthorizationModule();
-    private static int counter = 0;
-    private static List<String> generatedIDs = new ArrayList<>();
+    private static final Random random = new Random();
+    private static Set<String> generatedIDs = new HashSet<>();
 
     public static AtmImpl generateAtm(State initialState, String atmID) {
         if (atmID == null) {
-            counter++;
-            atmID = "atm" + counter;
+            atmID = generateID();
         }
         generatedIDs.add(atmID);
         return new AtmImpl(initialState, authorization, atmID);
@@ -20,5 +18,17 @@ public class AtmBuilder {
 
     public static List<String> getGeneratedIDs(){
         return new ArrayList<>(generatedIDs);
+    }
+
+    public static String generateID() {
+        boolean idGenerated = false;
+        String atmID = "";
+        while (!idGenerated) {
+            atmID = "atm" + random.nextInt(111111111);
+            if (!generatedIDs.contains(atmID)) {
+                idGenerated = true;
+            }
+        }
+        return atmID;
     }
 }
